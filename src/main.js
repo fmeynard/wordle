1180416cabbdfc0f90abd3a2ca18cbcab571e4e9
Fabien MEYNARD <fabien.meynard@gmail.com>
2022-02-22 21:30:58 +0000

    polish

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const cliProgress = require('cli-progress');
 function countLettersDic(str) {
     let dic = {};
     for(let i=0; i<5;i++) {
-        dic[str[i]] = typeof dic[str[i]] == 'undefined' ? 1 : dic[str[i]]+1;
+        dic[str[i]] = (dic[str[i]] ?? 0) + 1;
     }
 
     return dic;
@@ -18,21 +18,21 @@ function getGuessScore (word, guess) {
     for(let i=0; i<5;i++) {
         let letter = guess[i];
         if (letter === word[i]) {
-            score += 2;
+            score += '2';
             toFind[letter]--;
             continue;
         }
 
         if (word.indexOf(letter) < 0) {
-            score += 0;
+            score += '0';
             continue;
         }
 
         if (toFind[letter] > 0) {
-            score += 1;
+            score += '1';
             toFind[letter]--;
         } else {
-            score += 0;
+            score += '0';
         }
     }
 
@@ -42,12 +42,12 @@ function getGuessScore (word, guess) {
 
 function getMatrix(words, initialGuess = null) {
     let patternsMatrix = {};
-
-    const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-    let x=0;
-    bar.start(words.length, 0);
     let guessScoreSet = new Set();
     const guesses = initialGuess === null ? words : [initialGuess];
+    
+    const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+    let x = 0;
+    bar.start(words.length, x);
     for (let word of words) {
         for (let guess of guesses) {
             const score = getGuessScore(word, guess);
@@ -59,7 +59,6 @@ function getMatrix(words, initialGuess = null) {
                 patternsMatrix[key].push(word);
             }
         }
-
         bar.update(++x);
     }
     bar.stop();
@@ -85,45 +84,54 @@ function getInitialMatrix(filePath, initialGuess = null) {
 function getResults(patternsMatrix) {
     let ranks = [];
     for (let key in patternsMatrix) {
-        let [guess,] = key.split('_');
+        const [guess,] = key.split('_');
 
-        if (!ranks.hasOwnProperty(guess)) {
-            ranks[guess] = {
-                words:  patternsMatrix[key].length,
-                count: 1
-            }
-        } else {
-            ranks[guess]['words'] += patternsMatrix[key].length;
-            ranks[guess]['count']++;
-        }
+        ranks[guess] = {
+            words:  (ranks[guess]?.words ?? 0) + patternsMatrix[key].length,
+            count: (ranks[guess]?.count ?? 0) + 1
+        };
     }
 
     return Object.keys(ranks)
-        .map(guess => { return { guess, score: ranks[guess]['words']/ranks[guess]['count'] }; })
+        .map(guess => { return { guess, score: +(ranks[guess]['words']/ranks[guess]['count']).toFixed(2) }; })
         .sort((a,b) => a.score - b.score)
         .slice(0, 10);
 }
 
 async function play() {
-    initialGuess = prompt('Initial guess :');
+    initialGuess = prompt('Initial guess ( enter to compute everything ) :');
     if (initialGuess.length === 0) {
         initialGuess = null;
     }
     const initialMatrix = getInitialMatrix('./data/combined.txt', initialGuess);
 
     let matrix = initialMatrix;
+    let tries = 0;
     while (true) {
         console.table(getResults(matrix));
 
-        word = prompt('Word :');
-        if (word.length === 0) {
-            console.log('reset game');
+        if (tries === 0) {
+            word = initialGuess;
+        } else {
+            word = prompt('Word : ');
+            if (word.length === 0) {
+                console.log('reset game');
+                matrix = initialMatrix;
+                tries = 1;
+                continue;
+            }
+        } 
+        tries++;
+
+        score = prompt('Score : ');
+        if (score === '22222' || score === '') { 
+            console.log(`Winning word ${word} in ${x} tries`);
+            console.log('-------');
+            console.log('New game !');
             matrix = initialMatrix;
+            tries = 1;
             continue;
         }
-
-        score = prompt('SCORE :');
-
         const key = word + '_' + score;
         if (typeof matrix[key] == 'undefined') {
             console.log('word not found', { word, score, key });
